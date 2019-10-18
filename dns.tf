e44d5009dc2d5c3e6c59e0c508b9c2f1aeae036f
Robert Maury <daquinox@gmail.com>
2019-10-18 12:51:03 -0700

    break out configurations into logical files

---
+++ dns.tf
@@ -0,0 +1,7 @@
+
+    #  data.aws_instance.pe-xl-core-0.public_ip
+    #  data.aws_instance.pe-xl-core-1.public_ip
+    #  data.aws_instance.pe-xl-core-2.public_ip
+    #  data.aws_instance.pe-xl-core-3.public_ip
+    #  data.aws_instance.pe-xl-compiler-0.public_ip
+    #  data.aws_instance.pe-xl-compiler-1.public_ip
---
+++ network.tf
@@ -0,0 +1,114 @@
+resource "aws_vpc" "pe_xl-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "pe_xl-sg" {
+  description = "Puppet Network Flow"
+  name        = "pe_xl-sg"
+  vpc_id      = "pe_xl-vpc"
+
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # ingress {
+  #   from_port   = 1
+  #   to_port     = 1
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # ingress {
+  #   from_port = 0
+  #   to_port   = 0
+  #   protocol  = "-1"
+  #   self      = "True"
+  # }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8140
+    to_port     = 8140
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8170
+    to_port     = 8170
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8123
+    to_port     = 8123
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8142
+    to_port     = 8142
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8143
+    to_port     = 8143
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 4433
+    to_port     = 4433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
