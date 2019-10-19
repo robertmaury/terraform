304e128f19be5a16f2d815e22fea7b9767429a34
Robert Maury <daquinox@gmail.com>
2019-10-19 13:35:32 -0700

    local custom provider for namecheap

--- dns.tf
+++ dns.tf
@@ -1,7 +1,56 @@
+provider "namecheap" {
+  version = "~> v1.4"
+  username = "daquinox"
+  api_user = "daquinox" # Same as username
+  token = "your_token"
+  ip = "1.2.3.4"
+  use_sandbox = false # Toggle for testing/sandbox mode
+}
 
-    #  data.aws_instance.pe-xl-core-0.public_ip
-    #  data.aws_instance.pe-xl-core-1.public_ip
-    #  data.aws_instance.pe-xl-core-2.public_ip
-    #  data.aws_instance.pe-xl-core-3.public_ip
-    #  data.aws_instance.pe-xl-compiler-0.public_ip
-    #  data.aws_instance.pe-xl-compiler-1.public_ip
+# resource "namecheap_record" "pe-xl-core-0" {
+#   name = "pe-xl-core-0"
+#   domain = "robertmaury.com"
+#   address = ${data.aws_instance.pe-xl-core-0.public_ip}
+#   mx_pref = 10
+#   type = "A"
+# }
+
+# resource "namecheap_record" "pe-xl-core-1" {
+#   name = "pe-xl-core-1"
+#   domain = "robertmaury.com"
+#   address = ${data.aws_instance.pe-xl-core-1.public_ip}
+#   mx_pref = 10
+#   type = "A"
+# }
+
+# resource "namecheap_record" "pe-xl-core-2" {
+#   name = "pe-xl-core-2"
+#   domain = "robertmaury.com"
+#   address = ${data.aws_instance.pe-xl-core-2.public_ip}
+#   mx_pref = 10
+#   type = "A"
+# }
+
+# resource "namecheap_record" "pe-xl-core-3" {
+#   name = "pe-xl-core-3"
+#   domain = "robertmaury.com"
+#   address = ${data.aws_instance.pe-xl-core-3.public_ip}
+#   mx_pref = 10
+#   type = "A"
+# }
+
+# resource "namecheap_record" "pe-xl-compiler-0" {
+#   name = "pe-xl-compiler-0"
+#   domain = "robertmaury.com"
+#   address = ${data.aws_instance.pe-xl-compiler-0.public_ip}
+#   mx_pref = 10
+#   type = "A"
+# }
+
+# resource "namecheap_record" "pe-xl-compiler-1" {
+#   name = "pe-xl-compiler-0"
+#   domain = "robertmaury.com"
+#   address = ${data.aws_instance.pe-xl-compiler-1.public_ip}
+#   mx_pref = 10
+#   type = "A"
+# }
